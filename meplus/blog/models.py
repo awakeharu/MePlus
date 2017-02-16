dfbfa6e0bd7e9a2ce22d79a55b96c156b56ebecd
Ohdonghwan <Ohdongdong@Ohdonghwanui-MacBook-Pro.local>
2017-02-17 00:43:15 +0900

    registerremoved

--- meplus/blog/models.py
+++ meplus/blog/models.py
@@ -14,25 +14,7 @@ class Post_Notice(models.Model):
     def __str__(self):
         return self.title
 
-
-class Post_Sale(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, limit_choices_to={"staff": True})
-    title = models.CharField(max_length=30)
-    contents = models.TextField()
-    image = models.ImageField(null=True, blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return self.title
-
-class Post_Sale_comment(models.Model):
-    name = models.ForeignKey(settings.AUTH_USER_MODEL)
-    question = models.CharField(max_length=1000)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-
+    
 class Contact(models.Model):
     name = models.ForeignKey(settings.AUTH_USER_MODEL)
     email = models.EmailField(max_length=70,blank=False)

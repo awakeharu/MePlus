9bbff1d197cf5053fc8be268f8bda3f7739ba145
awakeharu <haru_o@naver.com>
2017-02-17 00:16:15 +0900

    모델가요

--- meplus/blog/models.py
+++ meplus/blog/models.py
@@ -1,3 +1,59 @@
 from django.db import models
+from django.conf import settings
+from renamefile import RenameFile
+import os
 
-# Create your models here.
+
+class Notice(models.Model):
+    manager_name = models.ForeignKey(settings.AUTH_USER_MODEL, limit_choices_to={"admin": True})
+    title = models.CharField(max_length=30)
+    contents = models.TextField()
+    image = models.ImageField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Sale(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, limit_choices_to={"staff": True})
+    title = models.CharField(max_length=30)
+    contents = models.TextField()
+    image = models.ImageField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+class Sale_comment(models.Model):
+    name = models.ForeignKey(settings.AUTH_USER_MODEL)
+    question = models.CharField(max_length=1000)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Contact(models.Model):
+    name = models.ForeignKey(settings.AUTH_USER_MODEL)
+    email = models.EmailField(max_length=70,blank=False)
+    massage = models.TextField()
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Author(models.Model):
+    uploader = models.ForeignKey(settings.AUTH_USER_MODEL, limit_choices_to={"admin": True})
+    author = models.CharField()
+    explain = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.author
+
+
+
+
+
+
+
